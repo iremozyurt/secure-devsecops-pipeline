3b6f630ae9988f20fe46e988659546e9c8d3f32a
irem.ozyurt <iremel2211@hotmail.com>
2026-01-08 15:59:17 +0300

    Fix command injection vulnerability

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, request
 import subprocess
+import re
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "DevSecOps Pipeline Test App"
 
 @app.route("/ping")
 def ping():
     host = request.args.get("host", "127.0.0.1")
-    return subprocess.getoutput(f"ping -c 1 {host}")
+
+    # sadece IP adresine izin ver
+    if not re.match(r"^[0-9\.]+$", host):
+        return "Invalid host", 400
+
+    result = subprocess.run(
+        ["ping", "-c", "1", host],
+        capture_output=True,
+        text=True
+    )
+
+    return result.stdout
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-#Kullanıcıdan gelen host değeri hiç kontrol edilmeden
-#Shell komutunun içine giriyor
